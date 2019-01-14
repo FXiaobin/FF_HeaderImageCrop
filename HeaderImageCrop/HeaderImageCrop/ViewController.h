d3122688f5dd8bcb55594d7b17845cee48909d47
FanXiaobin <527256662@qq.com>
2019-01-14 16:09:37 +0800

    first commit
    first commit

---
+++ HeaderImageCrop/HeaderImageCrop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeaderImageCrop
+//
+//  Created by mac on 2019/1/14.
+//  Copyright © 2019 healifeGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
